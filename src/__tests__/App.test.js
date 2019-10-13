bb694cdba6ac26e215b94c57403cc0ad828388b5
Su3D <3333sd@gmail.com>
2019-10-12 20:07:45 -0400

    add CitySearch tests and component

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
+
 import EventList from '../EventList';
+import CitySearch from '../CitySearch';
 
 describe('<App /> component', () => {
+  //sets AppWrapper to use the shallow rendering API from Enzyme
+  //for all tests
+  let AppWrapper;
+  beforeAll(() => {
+    AppWrapper = shallow(<App />);
+  });
+
+  //test that EventList exists in the App component
   test('render list of events', () => {
-    const AppWrapper = shallow(<App />);
     expect(AppWrapper.find(EventList)).toHaveLength(1);
   });
+
+  //test that CitySearch exists in the App component
+  test('render CitySearch', () => {
+    expect(AppWrapper.find(CitySearch)).toHaveLength(1);
+  });
+
 });
---
+++ src/__tests__/CitySearch.test.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import CitySearch from '../CitySearch';
+
+describe('<CitySearch /> component', () => {
+  //sets CitySearchWrapper to use the shallow rendering API from Enzyme
+  //for all tests
+  let CitySearchWrapper;
+  beforeAll(() => {
+    CitySearchWrapper = shallow(<CitySearch />);
+  });
+
+  //tests that an element with the class .city exists in the CitySearch component
+  test('render text input', () => {
+    expect(CitySearchWrapper.find('.city')).toHaveLength(1);
+  });
+
+  //test that an element with the class name .suggestions exixts in the CitySearch component
+  test('render list of suggestions', () => {
+    expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
+  });
+
+  //compares search query with name of each city suggestion in list of cities
+  //only passes if they match
+  test('render text input correctly', () => {
+    const query = CitySearchWrapper.state('query'); //sets to query user types into textbox
+    expect(CitySearchWrapper.find('.city').prop('value')).toBe(query);
+  });
+
+  //test that the state is changed when the user enters text in the input field 
+  test('change state when text input changes', () => {
+    const eventObject = { target: { value: 'Denver' } }; //change value to Denver when change event is called
+    CitySearchWrapper.find('.city').simulate('change', eventObject); //simulates change on city - changing it to the target value: 'Denver'
+    expect(CitySearchWrapper.state('query')).toBe('Denver');
+  });
+
+  //verify list of suggestions rendered matches list of suggestions in component state 
+  test('render list of suggestions correctly', () => {
+    const suggestions = CitySearchWrapper.state('suggestions');
+    expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(suggestions.length);
+    for (let i = 0; i < suggestions.length; i += 1) {
+      expect(CitySearchWrapper.find('.suggestions li').at(i).text()).toBe(suggestions[i].name_string);
+    }
+  });
+
+  //test if value of query’s state changes when the user clicks on a suggested city
+  test('click on suggestion should change query state', () => {
+    CitySearchWrapper.setState({
+      suggestions: [
+        {
+          "city": "Arvada",
+          "country": "us",
+          "localized_country_name": "USA",
+          "state": "CO",
+          "name_string": "Arvada, Colorado, USA",
+          "zip": "80001",
+          "lat": 39.8,
+          "lon": -105.09
+        },
+        {
+          "city": "Arvada",
+          "country": "us",
+          "localized_country_name": "USA",
+          "state": "WY",
+          "name_string": "Arvada, Wyoming, USA",
+          "zip": "82831",
+          "lat": 44.71,
+          "lon": -106.1
+        }
+      ]
+    });
+
+    CitySearchWrapper.find('.suggestions li').at(0).simulate('click');
+    expect(CitySearchWrapper.state('query')).toBe('Arvada, Colorado, USA');
+  });
+});
