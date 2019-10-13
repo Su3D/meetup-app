ce00933208a8448efb8f2f7f9da22339234a4c78
Su3D <3333sd@gmail.com>
2019-10-13 17:38:31 -0400

    fix api calls

--- src/api.js
+++ src/api.js
@@ -2,29 +2,6 @@ import axios from 'axios';
 
 import { mockEvents } from './mock-events';
 
-//check users local storage for access_token
-const accessToken = localStorage.getItem('access_token');
-
-//IF no access_token is found check for code, if no code is found get it from Meetup
-if (!accessToken) {
-  const searchParams = new URLSearchParams(window.location.search);
-  const code = searchParams.get('code');
-  if (!code) {
-    window.location.href = 'https://secure.meetup.com/oauth2/authorize?client_id=bqk31njs5dm7oekb196m6l7c44&response_type=code&redirect_uri=https://su3d.github.io/meetup-app/';
-    return null;
-  }
-  return getOrRenewAccessToken('get', code);
-}
-
-//IF access_token is found in local storage is it still valid?
-const lastSavedTime = localStorage.getItem('last_saved_time');
-if (accessToken && (Date.now() - lastSavedTime < 3600000)) {
-  return accessToken;
-}
-
-//IF access_token is expired renew it via refresh_token
-const refreshToken = localStorage.getItem('refresh_token');
-return getOrRenewAccessToken('renew', refreshToken);
 
 //get OR renew access_token
 async function getOrRenewAccessToken(type, key) {
@@ -38,18 +15,43 @@ async function getOrRenewAccessToken(type, key) {
     url = 'https://x378hskxk6.execute-api.us-east-1.amazonaws.com/dev/api/refresh/'
       + key;
   }
-
   // Use Axios to make a GET request to the endpoint
   const tokenInfo = await axios.get(url);
-
   // Save tokens to localStorage together with a timestamp
   localStorage.setItem('access_token', tokenInfo.data.access_token);
   localStorage.setItem('refresh_token', tokenInfo.data.refresh_token);
   localStorage.setItem('last_saved_time', Date.now());
-
   // Return the access_token
   return tokenInfo.data.access_token;
 }
+
+
+async function getAccessToken() {
+  //check users local storage for access_token
+  const accessToken = localStorage.getItem('access_token');
+
+  //IF no access_token is found check for code, if no code is found get it from Meetup
+  if (!accessToken) {
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get('code');
+    if (!code) {
+      window.location.href = 'https://secure.meetup.com/oauth2/authorize?client_id=bqk31njs5dm7oekb196m6l7c44&response_type=code&redirect_uri=https://su3d.github.io/meetup-app/';
+      return null;
+    }
+    return getOrRenewAccessToken('get', code);
+  }
+
+  //IF access_token is found in local storage is it still valid?
+  const lastSavedTime = localStorage.getItem('last_saved_time');
+  if (accessToken && (Date.now() - lastSavedTime < 3600000)) {
+    return accessToken;
+  }
+
+  //IF access_token is expired renew it via refresh_token
+  const refreshToken = localStorage.getItem('refresh_token');
+  return getOrRenewAccessToken('renew', refreshToken);
+}
+
 
 
 
@@ -103,6 +105,7 @@ async function getEvents(lat, lon) {
   }
 
   //IF not on localhost ask for access_token and send GET request to real Meetup API
+  const token = await getAccessToken();
   if (token) {
     //endpoint for Meetup API "Upcoming Events" method
     let url = 'https://api.meetup.com/find/upcoming_events?&sign=true&photo-host=public'
